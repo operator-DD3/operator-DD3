24dd3c326d6e74518d181c85859f55111e55459a
Jacob Gardner <operator-DD3@users.noreply.github.com>
2014-06-18 16:07:01 -0500

    Update db.lua

--- db.lua
+++ db.lua
@@ -1,27 +1,42 @@
-entries = {
-   post0001 = {
-   title = "First Update : 06/18/2014",
-   org = "skvmb",
-   email = "operator.dd3@gmail.com",
-   contact = "Jacob Gardner",
-   description = [[
-I, Jacob Gardner am proud to present the First
-real update! This update brings multiple entry
-support.
-Stay tooned!]]
-   },
- 
-   entry = {
-   title = "Initial Commit : 06/17/2014",
-   org = "skvmb",
-   email = "operator.dd3@gmail.com",
-   contact = "Jacob Gardner",
-   description = [[
-skvCMS is a very simple Content Management System
-created in Lua. This page will reflect the updates,
-and revisions throughout the development process.
-skvCMS's birthday is today: June 17, 2014]]
-     
-     }
-     
-   }
+ entries={ 
+   post0003 = { 
+   series = 3, 
+   title = 'Check it out', 
+   description = [[This is now on github. 
+   Visit <a href="https://github.com/operator-dd3/operator-dd3">operator-dd3</a>]] 
+   }, 
+   post0002 = { 
+   series = 2, 
+   title = "External Database Support: 06/18/2014", 
+   org = "skvmb", 
+   email = "operator.dd3@gmail.com", 
+   contact = "Jacob Gardner", 
+   description = [[ 
+   This update adds support for an external 
+   database.]] 
+   }, 
+   post0001 = { 
+   series = 1, 
+   title = "First Update : 06/18/2014", 
+   org = "skvmb", 
+   email = "operator.dd3@gmail.com", 
+   contact = "Jacob Gardner", 
+   description = [[ 
+     I, Jacob Gardner am proud to present the First 
+     real update! This update brings multiple entry 
+     support. 
+     Stay tooned!]] 
+   }, 
+ post0000={ 
+   series = 0, 
+   title = "Initial Commit", 
+   org = "skvmb", 
+   url = "http://skvmb.github.com", 
+   contact = "Jacob Gardner", 
+   description = [[ 
+     LuaCMS is a very simple Content Management system 
+     created in Lua. This page will reflect the updates, 
+     and revisions throughout the development process. 
+     LuaCMS's birthday is today: June 17, 2014]] 
+   } 
+ } 
