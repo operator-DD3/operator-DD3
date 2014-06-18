b1fd1b4f7a5a05e0729ed948bf2bd9d37ccb6ce8
Jacob Gardner <operator-DD3@users.noreply.github.com>
2014-06-18 13:00:10 -0500

    Update dd3.lua
    Now supports external database file.

--- dd3.lua
+++ dd3.lua
@@ -1,32 +1,3 @@
- entries = {
-   post0001 = {
-   title = "First Update : 06/18/2014",
-   org = "skvmb",
-   email = "operator.dd3@gmail.com",
-   contact = "Jacob Gardner",
-   description = [[
-     I, Jacob Gardner am proud to present the First
-     real update! This update brings multiple entry
-     support.
-     
-     Stay tooned!]]
-   },
- 
-   entry = {
-   title = "Initial Commit : 06/17/2014", 
-   org = "skvmb", 
-   email = "operator.dd3@gmail.com", 
-   contact = "Jacob Gardner", 
-   description = [[ 
-     skvCMS is a very simple Content Management System 
-     created in Lua. This page will reflect the updates, 
-     and revisions throughout the development process.
-      
-     skvCMS's birthday is today: June 17, 2014]] 
-     
-     }
-     
-   } 
 
  
  function fwrite (fmt, ...) 
@@ -72,7 +43,9 @@
  end 
  
 -- MAIN PROGRAM 
-
+ local inputfile = "db.lua"
+ dofile(inputfile)
+ 
  writeheader() 
  count = 0 
  fwrite('<ul>\n') 
