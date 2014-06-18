783152954a17a3301482c25d17aee035bc691bdd
Jacob Gardner <operator-DD3@users.noreply.github.com>
2014-06-18 12:56:15 -0500

    Create db.lua
    This is the database of all the blog entries.

---
+++ db.lua
@@ -0,0 +1,27 @@
+entries = {
+   post0001 = {
+   title = "First Update : 06/18/2014",
+   org = "skvmb",
+   email = "operator.dd3@gmail.com",
+   contact = "Jacob Gardner",
+   description = [[
+I, Jacob Gardner am proud to present the First
+real update! This update brings multiple entry
+support.
+Stay tooned!]]
+   },
+ 
+   entry = {
+   title = "Initial Commit : 06/17/2014",
+   org = "skvmb",
+   email = "operator.dd3@gmail.com",
+   contact = "Jacob Gardner",
+   description = [[
+skvCMS is a very simple Content Management System
+created in Lua. This page will reflect the updates,
+and revisions throughout the development process.
+skvCMS's birthday is today: June 17, 2014]]
+     
+     }
+     
+   }
